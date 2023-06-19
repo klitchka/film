944ea3945c3e0f9bf350b3ed79ac889182521ae4
Ana Aparicio <aaparici@student.42madrid.com>
2023-06-19 21:47:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const { register, handleSubmit, reset } = useForm();
+  const [options, setOptions] = useState([]);
+  const [votes, setVotes] = useState({});
+
+  const onSubmit = (data) => {
+    if (data.option && !options.includes(data.option)) {
+      setOptions((oldOptions) => [...oldOptions, data.option]);
+      setVotes((oldVotes) => ({ ...oldVotes, [data.option]: 0 }));
+    }
+    reset();
+  };
+
+  const handleVote = (option) => {
+    setVotes((oldVotes) => ({ ...oldVotes, [option]: oldVotes[option] + 1 }));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("option")} required />
+        <button type="submit">Añadir opción</button>
+      </form>
+      <h2>película / film / 映画 / 영화 / фильм / filma</h2>
+      {options.map((option) => (
+        <div key={option}>
+          {option} - {votes[option]} votos
+          <button onClick={() => handleVote(option)}>Votar</button>
+        </div>
+      ))}
     </div>
   );
 }
